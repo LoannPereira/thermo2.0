604158e3cf60e7311cad01dcab1bac7f070b8c61
argiraud <argiraud@172.27.66.171>
2017-11-17 11:15:37 +0100

    création package

--- thermo2.0/src/launcher/Thermo20.java
+++ thermo2.0/src/launcher/Thermo20.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package thermo2.pkg0;
+package launcher;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
